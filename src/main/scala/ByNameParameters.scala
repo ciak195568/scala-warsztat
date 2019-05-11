37c479154999c7b9448d79400047b3c6d0fb57fa
Wiktor Sikora <195703@edu.p.lodz.pl>
2019-05-11 15:48:20 +0200

    fix nazw

--- src/main/scala/ByNameParameters.scala
+++ src/main/scala/ByNameParameters.scala
@@ -9,7 +9,7 @@ object ByNameParameters {
   }
 
   // TODO
-  // Stworzyć funkcję o nazwie “byValueExercise”
+  // Stworzyć funkcję o nazwie “byNameExercise”
   // zwracającą czas w jakim została obliczona wartość przekazana jako parametr by-name.
   // Przykład wywołania funkcji znajduje się w pliku “ByNameParametersTest”.
   // Do pobrania aktualnego czasu można wykorzystać funkcję “System.nanoTime()”
